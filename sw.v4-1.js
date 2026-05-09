03e66a804893404d0783a45d90716f7932d9148a
nikakrawivonok-coder <nika.krawivonok@gmail.com>
2026-05-09 11:10:24 +0300

    Delete sw.v4-1.js

--- sw.v4-1.js
+++
@@ -1,8 +0,0 @@
-const CACHE = "polovyi-modul-v4-1-cache-v2";
-const FILES = ["./", "./index.html", "./styles.v4-1.css", "./app.v4-1.js", "./manifest.webmanifest"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
