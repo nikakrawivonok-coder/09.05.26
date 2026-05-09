1e7e36f07dd5e664022b04c4fd71e2a2e2ff8563
nikakrawivonok-coder <nika.krawivonok@gmail.com>
2026-05-09 11:01:01 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = "polovyi-modul-v4-roles-cache-v1";
-const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
